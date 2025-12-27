700c1ac1c77617bb6df2adefd80b6ea25913741f
Ishant <vedakishant@gmail.com>
2025-12-27 10:46:03 -0800

    users can create account from login page.

--- App/tournament_app/users/urls.py
+++ App/tournament_app/users/urls.py
@@ -5,5 +5,5 @@ app_name = "users"
 urlpatterns = [
     path("", views.user, name="user"),
     path("all/", views.all_users, name="all_users"),
-    path("create/", views.create_user, name="create_users"),
+    path("create/", views.create_user, name="create"),
 ]
