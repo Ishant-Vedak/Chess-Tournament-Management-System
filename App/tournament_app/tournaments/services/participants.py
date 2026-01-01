1e28ae54b9e1abdb6b91a47147a41a96cd96cf2d
Ishant <vedakishant@gmail.com>
2026-01-01 12:56:25 -0800

    able to import participants from csv file, and created tests for it.

--- App/tournament_app/tournaments/services/participants.py
+++ App/tournament_app/tournaments/services/participants.py
@@ -1,11 +1,12 @@
 from tournaments.models import Tournament, Participant
 from tournaments.services.state import InvalidState
+import csv
 
 def add_participant(
-        t: Tournament, 
+        tournament: Tournament, 
         name: str, 
         email: str | None = None, 
-        rating: str | None = None,
+        rating: int | None = None,
         user=None
         ):
     
@@ -14,8 +15,8 @@ def add_participant(
 
     Checks if tournament registration is open. If it is, allows participants to be added. Otherwise, error.
     
-    :param t: Tournament which the participant will be added to.
-    :type t: Tournament
+    :param tournament: Tournament which the participant will be added to.
+    :type tournament: Tournament
     :param name: Name of participant imported, either through csv or json.
     :type name: str
     :param email: Email of participant, but optional.
@@ -24,7 +25,49 @@ def add_participant(
     :type rating: str | None
     :param user: If the participant is logged in, they can use their account. Currently not addressing this. 
     '''
-    tournament = t
-    if tournament.status != "REGISTRATION_OPEN":
+    t = tournament
+    if t.status != "REGISTRATION_OPEN":
         raise InvalidState("Registration is not open, Participants cannot be added.")
-    Participant.objects.create(name=name, tournament=tournament, email=email, rating=rating)
+    Participant.objects.create(name=name, tournament=t, email=email, rating=rating)
+
+def import_participants_from_csv(tournament: Tournament, file):
+    '''
+    Docstring for import_participants_from_csv
+
+    Imports all participants from a csv file provided by organizers. 
+    
+    :param tournament: Tournament for the participants. 
+    :type tournament: Tournament
+    :param file: CSV file containing names of participants. Must have a name column, and could have an email and rating column.
+    :type file: file-like object
+    '''
+    errors = []
+    import_count = 0
+    
+    reader = csv.DictReader(file)
+
+    if not "name" in reader.fieldnames:
+        raise ValueError("CSV File is missing column 'name' ")
+
+    for i, row in enumerate(reader, start=1):
+        name = row.get('name', '').strip()
+        email = row.get('email', '').strip() or None
+        rating = row.get('rating', '').strip() or None
+
+        if not name: 
+            errors.append(f'Row {i}: Missing name')
+            continue
+
+        if rating is not None:
+            try: 
+                rating = int(rating)
+            except ValueError:
+                errors.append(f'Row {i}: Invalid Rating "{row.get('rating')}".')
+                continue
+        try: 
+            add_participant(tournament=tournament, name=name, email=email, rating=rating)
+            import_count += 1 
+        except InvalidState as e:
+            errors.append(f'Row {i}: {str(e)}')
+            
+    return import_count, errors
--- App/tournament_app/tournaments/tests/test_participants.py
+++ App/tournament_app/tournaments/tests/test_participants.py
@@ -3,6 +3,8 @@ from users.models import User
 from tournaments.models import Tournament, Participant
 from tournaments.services import participants, state
 from tournaments.services.state import InvalidState
+import csv
+import tempfile
 
 class ParticipantTestCase(TestCase):
     def setUp(self):
@@ -12,7 +14,7 @@ class ParticipantTestCase(TestCase):
     def testing_add_participants_with_open_reg(self):
         tournament=Tournament.objects.get(name='WigglyWobbly')
         state.open_registration(tournament)
-        participants.add_participant(t=tournament, name="Challenger")
+        participants.add_participant(tournament=tournament, name="Challenger")
         participant = Participant.objects.get(name="Challenger")
         self.assertEqual(str(participant), "Challenger in WigglyWobbly.")
     def testing_add_participants_with_closed_reg(self):
@@ -20,4 +22,36 @@ class ParticipantTestCase(TestCase):
         state.open_registration(tournament)
         state.close_registration(tournament)
         with self.assertRaises(InvalidState):
-            participants.add_participant(t=tournament, name='Challenger2')
+            participants.add_participant(tournament=tournament, name='Challenger2')
+    def testing_import_participants_with_correct_format(self):
+        tournament=Tournament.objects.get(name='WigglyWobbly')
+        state.open_registration(tournament)
+        with tempfile.NamedTemporaryFile(mode='w+', newline='', delete=True) as tmp:
+            fieldnames = ['name', 'email']
+            writer = csv.DictWriter(tmp, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'name': 'Dane', 'email': 'Sperling'})
+            writer.writerow({'name': 'Ryder', 'email': 'Hsu'})
+            writer.writerow({'name': 'Hudson', 'email': 'Regis'})
+            tmp.seek(0)
+            import_count, errors = participants.import_participants_from_csv(
+                tournament=tournament,
+                file=tmp
+            )
+            self.assertEqual(import_count, 3)
+            self.assertEqual(errors, [])
+    def testing_import_participants_with_wrong_format(self):
+        tournament=Tournament.objects.get(name='WigglyWobbly')
+        state.open_registration(tournament)
+        with tempfile.NamedTemporaryFile(mode='w+', newline='', delete=True) as tmp:
+            fieldnames = ['email', 'rating']
+            writer = csv.DictWriter(tmp, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'email': 'ds@gmail.com', 'rating': 2000})
+            writer.writerow({'email': 'rh@gmail.com', 'rating': 2001})
+            writer.writerow({'email': 'hr@gmail.com', 'rating': 9000})
+            tmp.seek(0)
+            with self.assertRaises(ValueError):
+                participants.import_participants_from_csv(tournament=tournament, file=tmp)
+
+            
